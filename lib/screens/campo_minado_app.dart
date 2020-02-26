5b668f133a19a97ae757fe9ac0cf29b66dd68125
Carlos Roberto Barbosa Júnior <carlosjr.if@gmail.com>
2020-02-26 14:17:21 -0300

    [ADD] Adicionando a appbar de resultado

---
+++ lib/screens/campo_minado_app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../components/resultado_widget.dart';
+
+class CampoMinadoApp extends StatelessWidget {
+  _reiniciar() {
+    print('reiniciar...');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: ResultadoWidget(
+          venceu: true,
+          onReiniciar: _reiniciar,
+        ),
+        body: Container(
+          child: Text('Tabuleiro'),
+        ),
+      ),
+    );
+  }
+}
