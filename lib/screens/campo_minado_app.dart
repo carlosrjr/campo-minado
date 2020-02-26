70322609a9ac7f38d1d322858f275946358defcb
Carlos Roberto Barbosa Júnior <carlosjr.if@gmail.com>
2020-02-26 14:48:11 -0300

    [ADD] Adicionando imagens para cada estado dos campos

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 import '../components/resultado_widget.dart';
+import '../components/campo_widget.dart';
+import '../models/campo.dart';
+import '../models/explosao_exception.dart';
 
 class CampoMinadoApp extends StatelessWidget {
-  _reiniciar() {
+  void _reiniciar() {
     print('reiniciar...');
   }
+  
+  _abrir(Campo campo) {
+    print('abrir...');
+  }
+  
+  _alternarMarcacao(Campo campo) {
+    print('Alternar Marcação...');
+  }
+
+  
 
   @override
   Widget build(BuildContext context) {
+    Campo vizinho1 = Campo(linha: 1, coluna: 1);
+    vizinho1.minar();
+    Campo vizinho2 = Campo(linha: 1, coluna: 0);
+    vizinho2.minar();
+    Campo vizinho3 = Campo(linha: 3, coluna: 3);
+    vizinho3.minar();
+  
+    Campo campo = Campo(linha: 0, coluna: 0);
+    campo.adicionarVizinho(vizinho1);
+    campo.adicionarVizinho(vizinho2);
+    campo.adicionarVizinho(vizinho3);
+
+    campo.altenarMarcacao();
+    try {
+      //campo.abrir();
+    } on ExplosaoException {}
+
+
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
@@ -15,7 +46,11 @@ class CampoMinadoApp extends StatelessWidget {
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: Text('Tabuleiro'),
+          child: CampoWidget(
+            campo: campo,
+            onAbrir: _abrir(campo),
+            onAlternarMarcacao: _alternarMarcacao(campo),
+          ),
         ),
       ),
     );
